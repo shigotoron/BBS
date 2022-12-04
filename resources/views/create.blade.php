07ac4ecb437c3b7424bd6f51e715981d845b12e8
Otias@GitHub <saito.sin.cos.tan@gmail.com>
2022-12-04 11:40:09 +0900

    タブの文字列の変更2

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,4 +1,6 @@
 <x-app-layout>
+    <x-slot name="title">新しい記事の作成</x-slot>
+
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             新しい記事の作成
--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,4 +1,6 @@
 <x-app-layout>
+    <x-slot name="title">記事の編集</x-slot>
+
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             記事の編集
