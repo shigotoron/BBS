e4ba38bfbadfeb67e2aa1697b42a44809e02d2f6
Otias@GitHub <saito.sin.cos.tan@gmail.com>
2022-12-18 16:17:41 +0900

    コントローラーに update アクションを追加

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -22,9 +22,6 @@ class CommentController extends Controller
         return redirect()->route('post', compact('post_id'));
     }
 
-    /**
-     * このアクションを追加
-     */
     public function edit($comment_id)
     {
         $comment = Comment::where('id', $comment_id)
@@ -32,5 +29,23 @@ class CommentController extends Controller
                           ->first();
 
         return view('edit_comment', compact('comment'));
+    }
+
+    /**
+     * このアクションを追加
+     */
+    public function update(Request $request, $comment_id)
+    {
+        $data = $request->all();
+        $query = Comment::where('id', $comment_id)->where('user_id', Auth::id());
+
+        // ユーザーが不正な操作で他人のコメントを編集しようとしていないかチェック
+        if ($query->exists()) {
+            $query->update(['content' => $data['content']]); // コメントの更新
+            $post_id = Comment::where('id', $comment_id)->first()->post->id; // 該当のコメントがつく記事の id を取得
+            return redirect()->route('post', compact('post_id')); // 該当の記事へリダイレクト
+        } else {
+            abort(500); // サーバーエラー
+        }
     }
 }
