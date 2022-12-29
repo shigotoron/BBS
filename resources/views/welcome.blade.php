6561b887b9bf290a4f27cc269353922d5e77539e
Otias@GitHub <saito.sin.cos.tan@gmail.com>
2022-12-29 18:57:01 +0900

    プログラミングと仕事論へのリンクを貼った

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,7 @@
 <x-guest-layout>
     <h1>Laravel掲示板</h1>
     <p>これはLaravelで作った電子掲示板です。</p>
+    <p>なお、この電子掲示板の作り方に関して知りたい方は、<a class="text-blue-500" href="https://shigotoron.com/laravel-%e3%81%a7%e9%9b%bb%e5%ad%90%e6%8e%b2%e7%a4%ba%e6%9d%bf%e3%82%92%e4%bd%9c%e3%82%8d%e3%81%86-%e2%91%a0/">こちら</a>を参照して下さい。</p>
     <h2>みんなの記事一覧</h2>
     <table class="table-auto border-solid border-black border-2" style="border-collapse: separate; border-spacing: 0">
         <tr class="bg-green-300">
